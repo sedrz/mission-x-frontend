0b277cdebd486f6e35ccae0c9dda324f0f757baa
sedrz <Raisianz@gmail.com>
2021-04-29 16:35:18 +1200

    fix router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from "react";
 
-import Navbar from './sharedComponents/Navbar/Navbar'
-import Footer from './sharedComponents/Footer/Footer'
-import Projects from "./pages/Projects/Projects"
+import Navbar from "./sharedComponents/Navbar/Navbar";
+import Footer from "./sharedComponents/Footer/Footer";
+import Router from "./Router";
 
 import "./App.css";
 
 function App() {
   return (
-      <div>
-        <Navbar />
-        <Projects/>
-        <Footer />
-      </div>
+    <>
+      <Navbar />
+      <Router />
+      <Footer />
+    </>
   );
 }
 export default App;
--- src/Router.js
+++ src/Router.js
@@ -1,38 +1,17 @@
-import React from 'react';
-import { Route,Switch } from 'react-router-dom';
-import App from './App';
-import TopPage from "./pages/TopPage";
-import Toptext from "./pages/Toptext"
-import Buttonend from "./pages/Buttonend"
-import SideCheckbox from "./pages/sidebarcheckbox"
-import Navbar from './Components/Navbar'
-import Banner from './Components/Banner'
-import Contentoffer from './Components/Contentoffer'
-import Contentb from './Components/Contentb'
-import Contentcomp from './Components/Contentcomp'
-import Contentstart from './Components/Contentstart'
-import Footer from './Components/Footer'
-import Projects from "./pages/Projects/Projects"
-//  * All routes go here.
-//  * Don't forget to import the components above after adding new route.
- function Router() {
-     return(
- <switch>
-    <Route exact path="/" component={App}>
-    <Route exat path="/Components/Banner" component={ Navbar} />
-    <Route exact path="/Components/Banner" component={ Banner} />
-    <Route exact path="/Components/Contentoffer" component={Contentoffer} />
-    <Route exact path="/Components/Contentb" component={Contentb} />
-    <Route exact path="/Components/Contentcomp" component={Contentcomp} />
-    <Route exact path="/pages/Toptext" component={Toptext} />
-    <Route exact path="/pages/TopPage" component={TopPage} />
-    <Route exact path="/Components/Contentstart" component={Contentstart} />
-    <Route exact path="/Components/Footer" component={Footer} />
-    <Route exact path="/pages/Projects/Projects" component={Projects} />
-    <Route exact path="/pages/sidebarcheckbox" component={SideCheckbox} />
-    <Route exact path="/pages/Buttonend" component={Buttonend} />
-  </Route>
-  </switch>
-);
- }
+import React from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+
+import Projects from "./pages/Projects/Projects";
+
+function Router() {
+  return (
+    <switch>
+      {/* <Route exact path="/">
+        <Redirect to="/home" />
+      </Route> */}
+      <Route exact path="/projects" component={Projects}></Route>
+      {/* <Route exact path="/home" component={Home}></Route> */}
+    </switch>
+  );
+}
 export default Router;
